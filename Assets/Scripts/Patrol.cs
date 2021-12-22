dc2e9ad22f2613da4e7ab68fbd2ef339f7a8bc5f
AntonioShen <1053619500@qq.com>
2021-12-22 15:19:15 -0500

    Adding moving from point A to point B

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    [SerializeField] Transform[] patrolPoints;
+    [SerializeField] float _speed = 1f;
+
+    Transform _target;
+
+    private void Start() {
+        _target = patrolPoints[0];
+    }
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _target.position, _speed * Time.deltaTime);
+    }
+}
