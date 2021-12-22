b0513855fab31b0d8cd961bbb406f187dc062d8e
AntonioShen <1053619500@qq.com>
2021-12-22 14:58:01 -0500

    Adding chasing behaviour

---
+++ Assets/Scripts/Chase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Collider))]
+[DisallowMultipleComponent]
+public class Chase : MonoBehaviour
+{
+    [SerializeField] float _speed = 3f;
+
+    Transform _target = null;
+    bool isActive = false;
+
+    void Update()
+    {
+        if (!isActive || _target == null)
+        {
+            return;
+        }
+        transform.LookAt(_target);
+        transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player"))
+        {
+            isActive = true;
+            _target = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player"))
+        {
+            isActive = false;
+            if (_target.transform == other.transform)
+            {
+                _target = null;
+            }
+        }
+    }
+}
